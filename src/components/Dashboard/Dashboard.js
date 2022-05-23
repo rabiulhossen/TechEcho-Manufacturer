84ca2d9ae9574a80dab4a94c03bf323a7c1cbe45
rabiulhossen <rhsimon69@gmail.com>
2022-05-23 15:53:36 +0600

    bug fixed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,18 +5,18 @@ export default function Dashboard() {
   return (
     <div>
 
-<div class="drawer drawer-mobile">
-  <input id="dash-sidebar" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content">
+<div className="drawer drawer-mobile">
+  <input id="dash-sidebar" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
   <h2 className='text-3xl font-serif text-slate-700 my-3 font-bold'>welcome to dashboard</h2>
 <Outlet></Outlet>
 
    
   
   </div> 
-  <div class="drawer-side">
-    <label for="dash-sidebar" class="drawer-overlay"></label> 
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+  <div className="drawer-side">
+    <label for="dash-sidebar" className="drawer-overlay"></label> 
+    <ul className="menu p-4 overflow-y-auto w-60 bg-base-100 text-base-content">
       {/* <!-- Sidebar content here --> */}
       <li><Link to="/dashboard">up</Link></li>
       <li><Link to="/dashboard/profile">portfolio</Link></li>
--- src/components/others/Blogs.js
+++ src/components/others/Blogs.js
@@ -6,58 +6,58 @@ export default function Blogs() {
 <h1 className='text-zinc-800 font-mono fw-bold my-8 text-2xl'>This is Blog section</h1>
 {/* <!-- The button to open modal --> */}
 <h1 className='font-bold text-lg'>differec betwwin?</h1>
-<label for="my-modal-5" class="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
+<label for="my-modal-5" className="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
 
 {/* <!-- Put this part before </body> tag --> */}
-<input type="checkbox" id="my-modal-5" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box w-11/12 max-w-5xl">
-    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div class="modal-action">
-      <label for="my-modal-5" class="btn">Close!</label>
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box w-11/12 max-w-5xl">
+    <h3 className="font-bold text-lg">Congratulations random Interner user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="modal-action">
+      <label for="my-modal-5" className="btn">Close!</label>
     </div>
   </div>
 </div>
 <h1 className='font-bold text-lg'>differec betwwin?</h1>
-<label for="my-modal-5" class="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
+<label for="my-modal-5" className="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
 
 {/* <!-- Put this part before </body> tag --> */}
-<input type="checkbox" id="my-modal-5" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box w-11/12 max-w-5xl">
-    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div class="modal-action">
-      <label for="my-modal-5" class="btn">Close!</label>
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box w-11/12 max-w-5xl">
+    <h3 className="font-bold text-lg">Congratulations random Interner user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="modal-action">
+      <label for="my-modal-5" className="btn">Close!</label>
     </div>
   </div>
 </div>
 <h1 className='font-bold text-lg'>differec betwwin?</h1>
-<label for="my-modal-5" class="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
+<label for="my-modal-5" className="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
 
 {/* <!-- Put this part before </body> tag --> */}
-<input type="checkbox" id="my-modal-5" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box w-11/12 max-w-5xl">
-    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div class="modal-action">
-      <label for="my-modal-5" class="btn">Close!</label>
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box w-11/12 max-w-5xl">
+    <h3 className="font-bold text-lg">Congratulations random Interner user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="modal-action">
+      <label for="my-modal-5" className="btn">Close!</label>
     </div>
   </div>
 </div>
 <h1 className='font-bold text-lg'>differec betwwin?</h1>
-<label for="my-modal-5" class="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
+<label for="my-modal-5" className="btn modal-button bg-secondary text-black hover:text-white">Show Answer</label>
 
 {/* <!-- Put this part before </body> tag --> */}
-<input type="checkbox" id="my-modal-5" class="modal-toggle" />
-<div class="modal">
-  <div class="modal-box w-11/12 max-w-5xl">
-    <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div class="modal-action">
-      <label for="my-modal-5" class="btn">Close!</label>
+<input type="checkbox" id="my-modal-5" className="modal-toggle" />
+<div className="modal">
+  <div className="modal-box w-11/12 max-w-5xl">
+    <h3 className="font-bold text-lg">Congratulations random Interner user!</h3>
+    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    <div className="modal-action">
+      <label for="my-modal-5" className="btn">Close!</label>
     </div>
   </div>
 </div>
