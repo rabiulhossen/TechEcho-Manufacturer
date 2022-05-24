848bca1a12b3ea3e155210022e785a7b3e38269b
rabiulhossen <rhsimon69@gmail.com>
2022-05-24 19:07:16 +0600

    portfolio all function done

--- src/components/MyPortfolio/Aboutme.js
+++ src/components/MyPortfolio/Aboutme.js
@@ -40,27 +40,42 @@ export default function Aboutme() {
 </div>
 
 
+<div>
+<h3 className='text-bold text-3xl mb-4'>My Skill</h3>
+
+<h4 className='text-bold text-xl text-primary'><span className='text-bold text-2xl'>Expert : </span>HTML5,CSS3,Bootstrap</h4>
+<h4 className='text-bold text-xl text-primary'><span className='text-bold text-2xl'>Intermediate : </span>Javascript,TailwindCss,ReactJs & Git</h4>
+<h4 className='text-bold text-xl text-primary'><span className='text-bold text-2xl'>Familiar : </span>Nodejs,Mongodb,Heruko</h4>
+<h4 className='text-bold text-xl text-primary'><span className='text-bold text-2xl'>Tools : </span>Vs code,Netlify,Github</h4>
+
+</div>
+<div>
+    <h1 className='text-3xl text-bold mb-3 mt-7'>Education</h1>
+    <h2 className='text-bold text-xl text-primary'>I have completed my HSC examination in 2019.Now I am Studing at University of creative Technology.</h2>
+</div>
+
                         </Col>
                         
                         <Col style={{background:'#1840428c'}} className='aboutMsg m-auto m-md-0 p-5' xs={10} sm={8} md={6}>
-                            <h2 className='mt-4'>About Me</h2>
-                            <p className='mt-4'>I am a Web developer. I build websites and web applications. I am comfortable with JavaScript. I also have experience working with Reactjs and Node js. I care about writing clean code. I am always dedicated and hardworking. I am always interested in learning new technology. </p>
+                            <h2 className='mt-4 text-3xl'>About Me</h2>
+                            <p className='mt-4 text-xl'>I am a Web developer. I build websites and web applications. I am comfortable with JavaScript. I also have experience working with Reactjs and Node js. I care about writing clean code. I am always dedicated and hardworking. I am always interested in learning new technology. </p>
                             <div className='d-flex align-items-center pt-4'>
-                                <h5 className='p-0 pr-2 m-0'>Name: </h5>
-                                <p className='p-0 m-0'> Rabiul Hoosen</p>
+                                <h5 className='p-0 pr-2 m-0 text-2xl'>Name: <span>Rabiul Hoosen</span> </h5>
+                              
                             </div>
-                            
-                            <div className='d-flex align-items-center pt-4'>
-                                <h5 className='p-0 pr-2 m-0'>Address: </h5>
-                                <p className='p-0 m-0'> Rangunia,Chittagong,Bangladesh</p>
+                            <div className='d-flex align-items-center pt-2 pb-2'>
+                                <h5 className='p-0 pr-2 m-0 text-2xl'>Phone : <span className='text-xl'>+8801883455693 </span> </h5>
+                               
                             </div>
-                            <div className='d-flex align-items-center pt-4'>
-                                <h5 className='p-0 pr-2 m-0'>Email: </h5>
-                                <p className='p-0 m-0'> rhsimon69@gmail.com</p>
+                           
+                            <div className='d-flex align-items-center pb-1'>
+                                <h5 className='p-0 pr-2 m-0 text-2xl'>Email: <span>rhsimon69@gmail.com</span> </h5>
+                              
                             </div>
-                            <div className='d-flex align-items-center pt-4 pb-4'>
-                                <h5 className='p-0 pr-2 m-0'>Phone: </h5>
-                                <p className='p-0 m-0'> +8801883455693</p>
+                           
+                            <div className='d-flex align-items-center pt-0'>
+                                <h5 className='p-0 pr-2 m-0 text-2xl'>Address: <span className='text-xl'>Rangunia,Chittagong,Bangladesh</span> </h5>
+                               
                             </div>
                             </Col>
                             </Row>
